c4c5ac20561dad971df1f4829e275204db7119ed
Fermin <fmfs@posteo.net>
2020-12-07 23:04:06 +0100

    Update company-maxima copyright header

--- company-maxima.el
+++ company-maxima.el
@@ -11,21 +11,20 @@
 ;; Package-Requires: ((emacs "25.1")(maxima "0.6.1")(seq "2.20")(company "0.9.13"))
 ;; License: GPL-3.0-or-later
 
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2 of
-;; the License, or (at your option) any later version.
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ;;
-;; This program is distributed in the hope that it will be
-;; useful, but WITHOUT ANY WARRANTY; without even the implied
-;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-;; PURPOSE.  See the GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public
-;; License along with this program; if not, write to the Free
-;; Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-;; MA 02111-1307 USA
-;;
+
 ;;; Commentary:
 ;; Maxima `company-mode' backend.
 ;; it uses the `maxima-mode' function `maxima-get-completions' with the
@@ -33,7 +32,6 @@
 ;;
 ;; It can also autocomplete libraries inside load with `maxima-get-libraries'.
 
-;; 
 
 ;;; Code:
 
