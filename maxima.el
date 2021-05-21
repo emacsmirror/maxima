0b60cccab8c526f31a9a9a513e726e46850bdd65
Daniel Nicolai <dalanicolai@gmail.com>
2021-05-21 11:16:17 +0200

    Add autoloads + company-mode hook func and update use-package form

--- maxima.el
+++ maxima.el
@@ -32,17 +32,28 @@
 
 ;; Quick intro
 ;;
-;; To install, put this file (as well as maxima-font-lock.el)
-;; somewhere in your Emacs load path.
-;; To make sure that `maxima.el' is loaded when necessary, whether to
-;; edit a file in maxima mode or interact with Maxima in an Emacs buffer,
-;; put the lines
+;; To install, put this file (as well as maxima-font-lock.el) somewhere in your
+;; Emacs load path. To make sure that `maxima.el' is loaded when necessary,
+;; whether to edit a file in maxima mode or interact with Maxima in an Emacs
+;; buffer, put the lines (or for use-package users see below)
 ;;  (autoload 'maxima-mode "maxima" "Maxima mode" t)
 ;;  (autoload 'maxima "maxima" "Maxima interaction" t)
 ;; in your `.emacs' file.  If you want any file ending in `.mac' to begin
 ;; in `maxima-mode', for example, put the line
 ;;  (setq auto-mode-alist (cons '("\\.mac" . maxima-mode) auto-mode-alist))
 ;; to your `.emacs' file.
+
+;; for users of use-package, the maxima package can be loaded with the form
+
+;; (use-package maxima
+;;   :init
+;;   (add-hook 'maxima-mode-hook #'maxima-hook-function)
+;;   (add-hook 'maxima-inferior-mode-hook #'maxima-hook-function)
+;;   (setq
+;; 	 org-format-latex-options (plist-put org-format-latex-options :scale 2.0)
+;; 	 maxima-display-maxima-buffer nil)
+;;   :mode ("\\.mac\\'" . maxima-mode)
+;;   :interpreter ("maxima" . maxima-mode))
 
 ;;; Code:
 
@@ -2031,6 +2042,7 @@ It uses BEG and END as a parameters."
 ;;;; Maxima mode
 
 ;; FIXME improve comment, add the "new" functions
+;;;###autoload
 (define-derived-mode maxima-mode prog-mode "Maxima" ()
   "Major mode for editing Maxima code.
 
@@ -2732,7 +2744,6 @@ Then go to the beginning of the next form."
      ["Kill process" maxima-stop t])))
 
 
-
 (define-derived-mode maxima-inferior-mode
   comint-mode
   "Inferior Maxima"
@@ -2785,9 +2796,14 @@ To scroll through previous commands,
                               'maxima-inferior-sentinel)))
   (set (make-local-variable 'comint-prompt-read-only) t))
 
+(defun maxima-hook-function ()
+  (when (require 'company-maxima nil t)
+    (add-to-list 'company-backends '(company-maxima-symbols company-maxima-libraries))
+    (company-mode-on)))
 
 ;;;; Running Maxima
 
+;;;###autoload
 (defun maxima ()
   "Run Maxima interactively inside a buffer."
   (interactive)
